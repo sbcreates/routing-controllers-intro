dccbdaffa833615129f8cd3e3191849ce3b29513
sbcreates <drawsarahb@gmail.com>
2017-08-30 17:21:52 -0400

    Defined @header instance variable for welcome, about, and contest page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,15 @@ class PagesController < ApplicationController
   # defines welcome method
   def welcome
     # render :about <-- would render 'about.erb' file on the welcome page. the render method is equivalent to running erb :about.
+    @header = "Hello and Welcome"
   end
 
   def about
+    @header = "About Us"
   end
 
   def contest
+    @header = "Contest Page"
   end
 
 end
