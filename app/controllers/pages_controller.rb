c42b807920a39b329e928709c0142be98189d13d
sbcreates <drawsarahb@gmail.com>
2017-08-30 17:00:17 -0400

    Initiated welcome and about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
 class PagesController < ApplicationController
+
+  # defines welcome method
+  def welcome
+    # render :about <-- would render 'about.erb' file on the welcome page. the render method is equivalent to running erb :about.
+  end
+
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#welcome'
+  #defined welcome route
+  get '/welcome' => 'pages#welcome'
+
+  get '/about' => 'pages#about'
 end
