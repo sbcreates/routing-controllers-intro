de37782754cd9f5241a2587fa7a48c46a683c933
sbcreates <drawsarahb@gmail.com>
2017-08-30 17:03:00 -0400

    Initiated contest page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#welcome'
-  #defined welcome route
+  # define welcome route
   get '/welcome' => 'pages#welcome'
-
+  # define about route
   get '/about' => 'pages#about'
+  # define contest route
+  get '/contest' => 'pages#contest'
+
 end
